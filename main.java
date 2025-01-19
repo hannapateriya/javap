57e47472315f28be84065eaa22fd53c0d116f795
Harnarayan <harnarayan@192.168.1.5>
2025-01-20 00:37:13 +0530

    hanna

---
+++ main.java
@@ -0,0 +1,14 @@
+class main {
+    // data member (also instance variable)
+    int id;
+
+    // data member (also instance variable)
+    String n;
+
+    public static void main(String args[]) {
+        // creating an object of Student
+        main s1 = new main();
+        System.out.println(s1.id);
+        System.out.println(s1.n);
+    }
+}
